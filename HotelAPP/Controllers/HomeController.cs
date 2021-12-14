22a7122eea3f17087e3f88cbd625fc173a49a775
ZamanovRenat <zamanov.renat@mail.ru>
2021-12-14 10:50:02 +0300

    Вывод на страницу данных из БД

--- HotelAPP/Controllers/HomeController.cs
+++ HotelAPP/Controllers/HomeController.cs
@@ -1,26 +1,26 @@
 ﻿using HotelAPP.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using HotelAPP.Context;
 
 namespace HotelAPP.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HotelContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, HotelContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Rooms.ToList());
         }
 
         public IActionResult Privacy()
