88cabe597be1339c51b3d49956e1d629094d392f
ZamanovRenat <zamanov.renat@mail.ru>
2022-01-28 14:36:25 +0300

    Добавление сущности юзер и роли

---
+++ HotelAPP/Domain/Entities/Identity/Role.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HotelAPP.Domain.Entities.Identity
+{
+    public class Role : IdentityRole
+    {
+        public const string Administrators = "Administrators";
+
+        public const string Users = "Users";
+    }
+}
