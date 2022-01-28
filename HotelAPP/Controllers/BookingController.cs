83841489fc5755bdf1bb237ce02471e10998488d
ZamanovRenat <zamanov.renat@mail.ru>
2022-01-28 09:09:24 +0300

    Подготовка контроллера и вьюшки для бронирования комнат

---
+++ HotelAPP/Controllers/BookingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelAPP.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
